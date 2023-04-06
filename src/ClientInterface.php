3625d152d99eafd41d23ff62fb04766be8e8d759
Haydar Kulekci <haydarkulekci@gmail.com>
2023-04-06 20:21:27 +0300

    client interfacet definition changes

--- src/ClientInterface.php
+++ src/ClientInterface.php
@@ -3,13 +3,21 @@
  * @since     Mar 2023
  * @author    Haydar KULEKCI <haydarkulekci@gmail.com>
  */
-
 namespace Qdrant;
 
+use Qdrant\Endpoints\Cluster;
 use Qdrant\Endpoints\Collections;
+use Qdrant\Endpoints\Service;
+use Qdrant\Endpoints\Snapshots;
 use Qdrant\Http\HttpClientInterface;
 
 interface ClientInterface extends HttpClientInterface
 {
     public function collections(string $collectionName = null): Collections;
+
+    public function snapshots(): Snapshots;
+
+    public function cluster(): Cluster;
+
+    public function service(): Service;
 }
